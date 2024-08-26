05991ba937fd9521f28bcee1e28965670aad47a3
afrin <mohdafrinnoushad@gmail.com>
2024-08-26 16:52:49 +0530

    Updated to use HashRouter for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar";
 import { Shop } from "./pages/shop/shop";
-import Contact from "./pages/contact"; 
+import Contact from "./pages/contact";
 import { Cart } from "./pages/cart/cart";
 import { ShopContextProvider } from "./context/shop-context";
 import { Footer } from "./components/footer";
 import Products from "./pages/products";
 
 function App() {
+  console.log("App component is rendering...");
+
   return (
     <div className="App">
       <ShopContextProvider>
